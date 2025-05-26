ed6561813a36c74f8cae9859aab52d42739ebdf0
kishan492 <121510518+kishan492@users.noreply.github.com>
2025-05-26 22:29:22 +0530

    Update utils.js
    samesite issue resolved

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,7 @@ export const generateToken=(userId,res)=>{
     res.cookie("jwt",token,{
         maxAge:7*24*60*60*1000,
         httpOnly:true,  //prevent xss attacks cross site scripting attacks
-        sameSite:"strict",  //csrf attacks cross-site request forgery attack
+        sameSite:process.env.NODE_ENV === "development" ? "lax" : "none", //csrf attacks cross-site request forgery attack
         secure:process.env.NODE_ENV !== "development",
     })
     return token; 
