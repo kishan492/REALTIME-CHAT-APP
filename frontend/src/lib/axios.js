278f01860a7aedf525f609d67a8bb1e6db37ec37
kishan492 <121510518+kishan492@users.noreply.github.com>
2025-05-26 22:10:29 +0530

    Update axios.js
    axios update

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
-  withCredentials: true,
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://realtime-chat-app-snwy.onrender.com/api",
+  withCredentials: true, // 🔥 this is critical for cookies
 });
